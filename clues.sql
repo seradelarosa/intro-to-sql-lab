80c3a27932046aca979edf0dfcdeaf5cd37180c8
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-30 03:41:54 -0400

    exercise 7 done.

--- clues.sql
+++ clues.sql
@@ -125,8 +125,6 @@ WHERE cities.id = (
 
 -- Answer: Brasilia
 
-
-
 ------------------------------------------------------------------------------
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -141,3 +139,9 @@ WHERE cities.id = (
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT * 
+FROM cities 
+WHERE population = (91084);
+
+-- Answer: Santa Monica CA!!
